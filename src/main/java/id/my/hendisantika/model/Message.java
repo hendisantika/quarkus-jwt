d773e9bc6ca371b99cdaf0086095df6ea290154d
hendisantika <hendisantika@yahoo.co.id>
2024-05-31 10:09:40 +0700

    Add Message🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/model/Message.java
@@ -0,0 +1,22 @@
+package id.my.hendisantika.model;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : quarkus-jwt
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/31/24
+ * Time: 10:09
+ * To change this template use File | Settings | File Templates.
+ */
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Message {
+    public String content;
+}
