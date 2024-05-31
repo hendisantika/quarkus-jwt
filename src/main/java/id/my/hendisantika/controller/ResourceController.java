1f78cfba37da8466801cead9fef740f200f026c8
hendisantika <hendisantika@yahoo.co.id>
2024-05-31 10:16:36 +0700

    Add ResourceController🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/controller/ResourceController.java
@@ -0,0 +1,17 @@
+package id.my.hendisantika.controller;
+
+import jakarta.ws.rs.Path;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : quarkus-jwt
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/31/24
+ * Time: 10:16
+ * To change this template use File | Settings | File Templates.
+ */
+@Path("/resource")
+public class ResourceController {
+}
