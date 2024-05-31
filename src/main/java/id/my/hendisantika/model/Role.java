51f0ef221221dc1bea47586508a13c6ab45becfa
hendisantika <hendisantika@yahoo.co.id>
2024-05-31 10:10:01 +0700

    Add Role Enum🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/model/Role.java
@@ -0,0 +1,15 @@
+package id.my.hendisantika.model;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : quarkus-jwt
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/31/24
+ * Time: 10:09
+ * To change this template use File | Settings | File Templates.
+ */
+public enum Role {
+    USER, ADMIN
+}
