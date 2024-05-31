d9af5ce8a81571160a759cf1cd2e2bf7a34e21f1
hendisantika <hendisantika@yahoo.co.id>
2024-05-31 10:12:17 +0700

    Add encode in PBKDF2Encoder util🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/util/PBKDF2Encoder.java
+++ src/main/java/id/my/hendisantika/util/PBKDF2Encoder.java
@@ -3,6 +3,12 @@ package id.my.hendisantika.util;
 import jakarta.enterprise.context.RequestScoped;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.util.Base64;
+
 /**
  * Created by IntelliJ IDEA.
  * Project : quarkus-jwt
@@ -22,4 +28,21 @@ public class PBKDF2Encoder {
     private Integer iteration;
     @ConfigProperty(name = "id.my.hendisantika.password.keyLength")
     private Integer keyLength;
+
+    /**
+     * More info (https://www.owasp.org/index.php/Hashing_Java) 404 :(
+     *
+     * @param cs password
+     * @return encoded password
+     */
+    public String encode(CharSequence cs) {
+        try {
+            byte[] result = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA512")
+                    .generateSecret(new PBEKeySpec(cs.toString().toCharArray(), secret.getBytes(), iteration, keyLength))
+                    .getEncoded();
+            return Base64.getEncoder().encodeToString(result);
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
 }
